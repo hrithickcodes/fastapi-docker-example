5599d8cbf943ca33edd293957b474258f3cd1a4d
hrithickcodes <hrithick.codes@gmail.com>
2024-07-12 23:54:40 +0530

    Let's cook FastAPI

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 
+# Initiate the app
 app = FastAPI()
 
-@app.get("/home")
-def root():
-    return {"message": "Hello from Docker!"}
+
+# The routes are called path in FastAPI
+@app.get("/")
+def hello():
+    return "Hello world!"
+
+# We can also have dynamic id
+@app.get("/data/{id}")
+def get_id(id : int):
+    return {f"Hey your id is {id}"}
+
